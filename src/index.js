d37ab452749b6dac17c50ea4413d7be940cac60d
Josélio <joseliobackup@gmail.com>
2025-03-29 18:54:42 -0300

    update projetc

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
-
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'; // Importe createRoot
 import App from './App';
 import './global.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 1. Selecione o elemento raiz
+const container = document.getElementById('root');
+
+// 2. Crie a raiz do React 18
+const root = createRoot(container);
+
+// 3. Renderize o app
+root.render(<App />);
